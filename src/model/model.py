450a7206510da537f66ca0a321b3af6740091f30
YasiruMethmal123 <yasiru.methmal@outlook.com>
2025-08-07 17:37:54 +0530

    build_model

---
+++ src/model/model.py
@@ -0,0 +1,5 @@
+def build_model(vocab_size, embedding_dim, max_length, num_classes):
+    """
+    Builds the deep learning model. This function would be in a file like
+    `src/models/model.py`.
+    """
